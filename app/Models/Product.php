23046e5539effe7d3162e469f111c6817988ba0a
Bokhtiar Tushar <bokhtiartoshar1@gmail.com>
2022-10-26 20:19:27 +0600

    product model create and migration done

---
+++ app/Models/Product.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\CrudTrait;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory, CrudTrait;
+    
+    /**database table with field */
+    protected $table = 'products';
+    protected $primaryKey = 'product_id';
+
+    protected $fillable = [
+        'name',
+        'body',
+        'price',
+        'image',
+        'category_id',
+        'sub_category_id',
+    ];
+
+    /**category reletion */
+    public function foodCategory()
+    {
+        return $this->belongsTo(FoodCategory::class, 'food_category_id', 'food_category_id');
+    }
+
+    public function foodSubCategory()
+    {
+        return $this->belongsTo(FoodSubCategory::class, 'food_sub_category_id', 'food_sub_category_id');
+    }
+} 
---
+++ database/migrations/2022_10_26_141237_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id('product_id');
+            $table->string('name')->require();
+            $table->string('image')->require();
+            $table->integer('price')->require();
+            $table->longText('body')->nullable();
+            $table->tinyInteger('status')->default(0);
+            $table->integer('category_id')->require();
+            $table->integer('sub_category_id')->require();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
