299b623a7294a71de1d435502f696f5e2a662498
Mamun-swe <mhmamun166009@gmail.com>
2022-11-01 18:44:48 +0600

    alert model and table and validation done

---
+++ app/Models/Aleart.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\CrudTrait;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Aleart extends Model
+{
+    use HasFactory, CrudTrait;
+
+    /**database table with field */
+    protected $table = 'alearts';
+    protected $primaryKey = 'aleart_id';
+
+    protected $fillable = [
+        'title',
+        'body',
+    ];
+
+}
